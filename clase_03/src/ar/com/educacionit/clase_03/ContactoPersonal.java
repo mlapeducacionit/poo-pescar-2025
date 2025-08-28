4f2af35900f4499858cef2dc2a98235e6a3806ea
Maxiliano Principe <mlapeducacionit@gmail.com>
2025-08-28 16:48:44 -0300

    Relación simple y refactor de las clase c

--- clase_03/src/ar/com/educacionit/clase_03/ContactoPersonal.java
+++ clase_03/src/ar/com/educacionit/clase_03/ContactoPersonal.java
@@ -6,22 +6,31 @@ public class ContactoPersonal {
 	private String nombre;
 	private String correo;
 	private Direccion direccion;
-	
-	
+		
 	// Constructor por defecto
 	public ContactoPersonal() {	}
 
-	// Constructor parametrizado 
+	// Constructor parametrizado (1)
 	public ContactoPersonal(String nombre, String correo) {
+		atributosComunes(nombre, correo);
+	}
+	
+	// Constructor parametrizado (2)
+	public ContactoPersonal(String nombre, String correo, Direccion direccion) {
+		atributosComunes(nombre, correo);
+		this.direccion = direccion;
+	}
+	// Btn Der -> Refactor -> Extract Method...
+	private void atributosComunes(String nombre, String correo) {
 		this.nombre = nombre;
 		this.correo = correo;
-	}
+	}	
 
 	// Métodos -> (Acciones) -> (Comportamiento)
 	void mostrarNombre() {
 		System.out.println("El nombre es: " + this.nombre);
 	}
-	
+
 	// Métodos estandar para poder acceder indirectamente a los atributos privados
 	public String getNombre() {
 		return nombre;
@@ -44,6 +53,14 @@ public class ContactoPersonal {
 			System.out.println("Se cambio, correctamente");
 		}
 		
+	}
+	
+	public Direccion getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(Direccion direccion) {
+		this.direccion = direccion;
 	}
 
 	@Override
