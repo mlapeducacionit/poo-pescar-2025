a47657a1b2a364bc195c24510bee64d7fd3de2aa
Maxiliano Principe <mlapeducacionit@gmail.com>
2025-08-28 15:06:02 -0300

    Agrego los archivos para subirlos

---
+++ clase_03/src/ar/com/educacionit/clase_03/Test.java
@@ -0,0 +1,13 @@
+package ar.com.educacionit.clase_03;
+
+public class Test {
+	// PUnto de entrada de la aplicación JAVA
+	
+	public static void main(String[] args) {
+		
+		// syso -> Ctrl + Enter
+		System.out.println("Hola mundo desde JAVA");
+		
+	}
+
+}
