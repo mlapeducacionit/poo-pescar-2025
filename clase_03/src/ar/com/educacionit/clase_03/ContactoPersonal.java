e07a7dbdad3f79a934a083160ad9bfbb85c88f0a
Maxiliano Principe <mlapeducacionit@gmail.com>
2025-08-28 15:25:26 -0300

    Agrego clase Contacto personal

---
+++ clase_03/src/ar/com/educacionit/clase_03/ContactoPersonal.java
@@ -0,0 +1,15 @@
+package ar.com.educacionit.clase_03;
+
+public class ContactoPersonal {
+
+	// Atributos (Caracteristas)
+	String nombre;
+	String correo;
+		
+	// Métodos -> (Acciones) -> (Comportamiento)
+	void mostrarNombre() {
+		System.out.println("El nombre es: " + this.nombre);
+	}
+	
+	
+}
